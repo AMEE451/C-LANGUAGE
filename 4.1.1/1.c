21c295247ace8db18cb154d0efcca15eb22ff56a
AMEE451 <desaiamee699@gmail.com>
2023-11-30 12:23:06 +0530

    30/11/2023

---
+++ 4.1.1/1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c;
+	
+	printf("Enter A :");
+	scanf("%d",&a);
+	printf("Enter B :");
+	scanf("%d",&b);
+	c=a;
+	a=b;
+	b=c;
+	printf("A :%d\n",a);
+	printf("B :%d",b);
+	
+}
