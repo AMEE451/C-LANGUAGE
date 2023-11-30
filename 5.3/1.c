9305e76f05ed7d401adcdac4267ec55df8a0cd32
AMEE451 <desaiamee699@gmail.com>
2023-11-30 12:38:57 +0530

    30/11/2023

---
+++ 5.3/1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+	int a,b,c;
+	printf("Enter A :");
+	scanf("%d",&a);
+	printf("Enter B :");
+	scanf("%d",&b);
+	printf("Enter C :");
+	scanf("%d",&c);
+	(a<b)
+		?(a<c)
+			?printf("a is small ")
+			:printf("c is small ")
+		:(b<c)
+			?printf("b is small")
+			:printf("c is small");	
+	return 0;	
+}
